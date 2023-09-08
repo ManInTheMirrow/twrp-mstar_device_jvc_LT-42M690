79384c8664c4eb40267fe6dc27368370c6e1cdde
ManInTheMirrow <75084095+ManInTheMirrow@users.noreply.github.com>
2023-09-08 14:02:13 +0300

    Update omni_komagome.mk

--- omni_komagome.mk
+++ omni_komagome.mk
@@ -6,8 +6,9 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
